62d6b473bbd5b47cc9ad81a884607d8797d5744e
hrzhu <andrei.zhu@gmail.com>
2011-12-17 21:01:53 +0800

    do one thing in a funtion

--- a5-3.c
+++ a5-3.c
@@ -4,12 +4,15 @@
 
 #include <stdio.h>
 
+void strcpy(char *s, char *t){
+	while (*s++ = *t++)
+		;
+}
 
 void strcat(char *s, char *t){
 	while (*s)
 		s++;
-	while (*s++ = *t++)
-		;
+	strcpy(s,t);
 }
 
 int main(){
