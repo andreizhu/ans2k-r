04a4a2a67442397d8987fae6faab90e76d030c6f
andrei.zhu <andrei.zhu@gmail.com>
2011-12-18 01:04:37 +0800

    gcc gives a warning:unknown escape sequence c

---
+++ a1-2.c
@@ -0,0 +1,10 @@
+/* Exercise 1-2. Experiment to find out what happens 
+ * when printf's argument string contains \c, 
+ * where c is some character not listed above.
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("test\c");
+}
