c080d196b8c66fd39aae1a19f34ecff356d3bf27
andrei.zhu <andrei.zhu@gmail.com>
2011-12-18 04:10:30 +0800

    a2e5-5

---
+++ a5-5.c
@@ -0,0 +1,66 @@
+/* Exercise 5-5. Write versions of the library functions strncpy, strncat, 
+ * and strncmp, which operate on at most the first n characters 
+ * of their argument strings. For example, strncpy(s,t,n) copies at most 
+ * n characters of t to s. Full descriptions are in Appendix B.
+ */
+
+#include <stdio.h>
+
+void strcpy(char *s, char *t){
+	while ( *s++ = *t++)
+		;
+}
+
+int strlen(char *s){
+	int len;
+
+	while (*s++)
+		len++;
+	return len;
+}
+
+char *strncpy(char *s, char *t, int n){
+	char *p = s;
+
+	if ( n <= strlen(t)){
+		for (;n > 0 && *(t+1); n--)
+			*s++ = *t++;
+	return p;
+	}
+}
+
+char *strncat(char *s, char *t, int n){
+	char *p=s;
+	
+	while (*s)
+		s++;
+	if ( n <= strlen(t))
+		for (; n > 0; n--)
+			*s++=*t++;
+	return p;
+}
+
+int strncmp(char *s, char *t, int n){
+	/*for (; *s==*t && n > 0; s++,t++,n--)
+		if (n==0)
+			return 0;
+	return *(s-1)-*(t-1);*/
+
+	int i;
+	
+	for ( i=1;i<=n;i++)
+		if (*s!=*t)
+			return *s-*t;
+	return 0;
+}
+
+int main(){
+	char s[20]="Make likes";
+	char *t="Syde";
+	char *u=" maths.";
+
+	printf("%s\n",strncpy(s,t,4));
+	printf("%s\n",strncat(s,u,6));
+	printf("%d\n", strncmp(s,"S",1));
+	return 0;
+}
